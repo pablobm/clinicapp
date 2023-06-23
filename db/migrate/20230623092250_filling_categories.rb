96aebbf15c462c3787de8547b9d9495fd5332dd7
curlybastard <onc.hub99@gmail.com>
2023-06-23 13:10:34 +0300

    filling_categories

--- db/migrate/20230623092250_filling_categories.rb
+++ db/migrate/20230623092250_filling_categories.rb
@@ -1,4 +1,7 @@
 class FillingCategories < ActiveRecord::Migration[7.0]
   def change
+    add_column :categories, :name, :string, index: {unique: true}
+    add_reference :doctors, :category, index: true, foreign_key: true
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_104901) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_092250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_104901) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -51,6 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_104901) do
     t.datetime "updated_at", null: false
     t.string "phone"
     t.string "full_name"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_doctors_on_category_id"
     t.index ["email"], name: "index_doctors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
   end
@@ -76,4 +79,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_104901) do
 
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "users"
+  add_foreign_key "doctors", "categories"
 end
