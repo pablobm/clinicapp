9eeb2845fa8fe8597d2ea9a1aa2452afb43519d6
Pablo Brasero <pablo@pablobm.com>
2023-09-01 22:31:19 +0100

    Lint

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,18 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Doctor, type: :model do
-   subject { Doctor.new(full_name: "Robert", phone: "1234567890", email: "doc1@gmail.com" )}
+  subject { Doctor.new(full_name: "Robert", phone: "1234567890", email: "doc1@gmail.com") }
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
 
   it "is not valid without a full_name" do
-    subject.full_name=nil
+    subject.full_name = nil
     expect(subject).to_not be_valid
   end
-  
+
   it "is not valid without a phone number" do
-    subject.phone=nil 
+    subject.phone = nil
     expect(subject).to_not be_valid
   end
 
@@ -21,12 +21,10 @@ RSpec.describe Doctor, type: :model do
   end
 
   it "is not valid if the phone number is not all digits" do
-    expect( (subject.phone).match? (/\A-?\d+\Z/) ).to eq(true)
+    expect((subject.phone).match?(/\A-?\d+\Z/)).to eq(true)
   end
 
-
- 
-it "is not valid without a phone number"    
-it "is not valid if the phone number is not 10 chars"   
-it "is not valid if the phone number is not all digits"
+  it "is not valid without a phone number"
+  it "is not valid if the phone number is not 10 chars"
+  it "is not valid if the phone number is not all digits"
 end
