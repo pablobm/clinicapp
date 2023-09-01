edc20a5173e7e21a41b1af8f5b83bd3603c13a33
Pablo Brasero <pablo@pablobm.com>
2023-09-01 22:30:46 +0100

    First working feature spec

---
+++ spec/factories/doctors.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :doctor do
+    full_name { "Olivia Octavius" }
+    phone { "0123456789" }
+    category
+  end
+end
--- spec/features/doctor/log_in_and_leave_recommendation_spec.rb
+++ spec/features/doctor/log_in_and_leave_recommendation_spec.rb
@@ -1,14 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Doctor loggin in", type: :feature do
-    # let(:doctor) { new (:doctor) }
-    @javascript
-    scenario "from main page", js: true do
-        visit root_path
-        click_button 'Are you a doctor?'
-        fill_in 'Phone', with: '1234567890'
-        fill_in 'Password', with: 'qwerty'
-        click_button 'Log in'
-        expect(page).to have_content 'Doctors public page'
-    end
+  scenario "from main page", js: true do
+    doctor = FactoryBot.create(:doctor, password: "password")
+    visit root_path
+    click_button "Are you a doctor?"
+    fill_in "Phone", with: doctor.phone
+    fill_in "Password", with: doctor.password
+    click_button "Log in"
+    expect(page).to have_content "Doctors public page"
+  end
 end
