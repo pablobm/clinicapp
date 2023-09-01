69fe335c15557a6c4d08c698c65a9cdb334b7802
Pablo Brasero <pablo@pablobm.com>
2023-09-01 22:48:22 +0100

    Use Faker

--- spec/factories/doctors.rb
+++ spec/factories/doctors.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :doctor do
-    full_name { "Olivia Octavius" }
+    full_name { Faker::Name.name }
     phone { "0123456789" }
     password { "password" }
     category
